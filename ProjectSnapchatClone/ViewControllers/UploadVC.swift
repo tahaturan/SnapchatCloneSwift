36901e566819b39fae89e379a0e83d1db498e534
tahaturan <tahaaturann16@gmail.com>
2023-05-11 16:01:31 +0300

    Forograf Secme Islemleri

--- ProjectSnapchatClone/ViewControllers/UploadVC.swift
+++ ProjectSnapchatClone/ViewControllers/UploadVC.swift
@@ -14,7 +14,9 @@ class UploadVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        uploadImageView.isUserInteractionEnabled = true
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(choosePicture))
+        uploadImageView.addGestureRecognizer(recognizer)
     }
     
 
@@ -22,4 +24,23 @@ class UploadVC: UIViewController {
     @IBAction func uploadButtonClicked(_ sender: Any) {
     }
     
+}
+
+
+//MARK: selected image for gallery
+extension UploadVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    @objc func choosePicture()  {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[.originalImage] as? UIImage
+        
+        uploadImageView.image = selectedImage
+        self.dismiss(animated: true)
+    }
 }
