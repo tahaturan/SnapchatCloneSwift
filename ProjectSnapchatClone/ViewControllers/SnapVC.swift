71c02f07d315ba721d8682803e2ba3d8f86fc597
tahaturan <tahaaturann16@gmail.com>
2023-05-11 18:44:48 +0300

    Image Slide Show kullanimi

--- ProjectSnapchatClone/ViewControllers/SnapVC.swift
+++ ProjectSnapchatClone/ViewControllers/SnapVC.swift
@@ -6,16 +6,39 @@
 //
 
 import UIKit
+import ImageSlideshow
+import ImageSlideshowSDWebImage
 
 class SnapVC: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
     var selectedSnap : SnapModel?
     var selectedTime : Int?
+    var inputArray = [SDWebImageSource]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         timeLabel.text = "Time Left: \(selectedTime ?? 0)"
+        
+        
+        if let snap  = selectedSnap {
+            for imageUrl in snap.imageArray{
+                inputArray.append(SDWebImageSource(urlString: imageUrl)!)
+            }
+            
+            let imageSlideShow = ImageSlideshow(frame: CGRect(x: 10, y: 10, width: self.view.frame.width * 0.95, height: self.view.frame.height * 0.7))
+            
+          let pageIndicator = UIPageControl()
+            pageIndicator.currentPageIndicatorTintColor = UIColor.lightGray
+            pageIndicator.pageIndicatorTintColor = .black
+            
+            imageSlideShow.pageIndicator = pageIndicator
+            imageSlideShow.backgroundColor = UIColor.white
+            imageSlideShow.contentScaleMode = UIViewContentMode.scaleAspectFit
+            imageSlideShow.setImageInputs(inputArray)
+            self.view.addSubview(imageSlideShow)
+            self.view.bringSubviewToFront(timeLabel)
+        }
     }
     
 
